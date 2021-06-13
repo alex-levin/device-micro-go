ca0aa9a6af6efc48010c5bf676d5320d1cd9b5bc
alex-levin <alexlevinb@gmail.com>
2021-06-13 07:37:02 -0400

    updated deployment.yml

---
+++ docker_clear.bat
@@ -0,0 +1,6 @@
+@echo off
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker stop %%i
+FOR /f "tokens=*" %%i IN ('docker ps -aq') DO docker rm -f %%i
+FOR /f "tokens=*" %%i IN ('docker images --format "{{.ID}}"') DO docker rmi -f %%i
+
+pause
